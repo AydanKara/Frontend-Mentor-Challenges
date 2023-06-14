818b7bb2de31da25f873675e90d9dc3a5d23a6d2
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-06-14 20:37:08 +0200

    refactoring, update, animation adding

--- space-tourism-website-main/starter-code/js/destination-moon.js
+++ space-tourism-website-main/starter-code/js/destination-moon.js
@@ -3,30 +3,29 @@ fetch("./js/data.json")
     return response.json();
   })
   .then((data) => {
-    console.log(data.destinations);
     const destination = data.destinations;
     const placeholderImage = document.querySelector("#data-image");
     const placeholderInfo = document.querySelector("#data-info");
     let outImage = "";
     let outInfo = "";
-    destination.forEach((info) => {
-      if (info.name === "Moon") {
-        outImage += `<img class="image-dest" src="${info.images.png}" alt="image">`;
-        outInfo += `<h2 class="heading-2">${info.name}</h2>
-            <p class="destination-desc">${info.description}</p>
+    console.log(destination[0].name);
+      if (destination[0].name === "Moon") {
+        outImage += `<img class="image-dest" src="${destination[0].images.png}" alt="image">`;
+        outInfo += `<h2 class="heading-2">${destination[0].name}</h2>
+            <p class="destination-desc">${destination[0].description}</p>
             <hr class="line">
             <div class="destination-extra-info">
               <div class="distance">
                 <span class="subheading-2">Avg. distance</span>
-                <span class="subheading-1">${info.distance}</span>
+                <span class="subheading-1">${destination[0].distance}</span>
               </div>
               <div class="travel">
                 <span class="subheading-2">Est. travel time</span>
-                <span class="subheading-1">${info.travel}</span>
+                <span class="subheading-1">${destination[0].travel}</span>
               </div>
         </div>`;
       }
-    });
+    
     placeholderImage.innerHTML = outImage;
     placeholderInfo.innerHTML = outInfo;
   });
--- space-tourism-website-main/starter-code/js/script.js
+++ space-tourism-website-main/starter-code/js/script.js
@@ -1,4 +1,4 @@
-
+/* Sidebar */
 
 function sidebarFunc() {
   //! header sidebar start
@@ -29,3 +29,32 @@ function sidebarFunc() {
 }
 
 sidebarFunc();
+
+/* Sidebar */
+
+/* Tabs functions */
+
+let tabs = document.querySelectorAll(".nav-item-link");
+let contents = document.querySelectorAll(".destination");
+let contentsImage = document.querySelectorAll(".destination-image");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    contentsImage.forEach((contentImage) => {
+      contentImage.classList.remove("active");
+    })
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+    tabs[index].classList.add("active");
+    contents[index].classList.add("active");
+    contentsImage[index].classList.add("active");
+
+
+
+  });
+});
