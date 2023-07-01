a0b49dcfe0c9c07c22b3c88cda60147c6fa6665f
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-07-01 23:02:36 +0200

    cart-container toggle functionality

--- ecommerce-product-page-main/js/script.js
+++ ecommerce-product-page-main/js/script.js
@@ -1,31 +1,37 @@
-function sidebarFunc() {
-    //! sidebar start
-    const btnOpenSidebar = document.querySelector("#btn-menu");
-    const sidebar = document.querySelector("#sidebar");
-    const closeSidebar = document.querySelector("#close-sidebar");
-  
-    btnOpenSidebar.addEventListener("click", function () {
-      sidebar.style.right = "0";
-      closeSidebar.style.display = "block";
-    });
-  
-    closeSidebar.addEventListener("click", function () {
-      sidebar.style.right = "-100%";
-      closeSidebar.style.display = "none";
-    });
-  
-    /* click outside start */
-    document.addEventListener("click", function (event) {
-      if (
-        !event.composedPath().includes(sidebar) &&
-        !event.composedPath().includes(btnOpenSidebar)
-      ) {
-        sidebar.style.right = "-100%";
-      }
-    });
-    /* click outside end */
-  
-    //! sidebar end
-  }
+//! sidebar start
 
-  sidebarFunc();
+function sidebarFunc() {
+  const btnOpenSidebar = document.querySelector("#btn-menu");
+  const sidebar = document.querySelector("#sidebar");
+  const closeSidebar = document.querySelector("#close-sidebar");
+
+  btnOpenSidebar.addEventListener("click", function () {
+    sidebar.style.right = "0";
+    closeSidebar.style.display = "block";
+  });
+
+  closeSidebar.addEventListener("click", function () {
+    sidebar.style.right = "-100%";
+    closeSidebar.style.display = "none";
+  });
+}
+
+sidebarFunc();
+
+//! sidebar end
+
+//! cart-container start
+
+const btnCartContainer = document.querySelector(".profile-cart");
+const cartCount = document.querySelector(".cart-count");
+const cartContainer = document.querySelector("#cart-container");
+
+cartCount.style.display = "none";
+
+function cartToggleFunc() {
+  cartContainer.classList.toggle("hidden");
+}
+
+btnCartContainer.addEventListener("click", cartToggleFunc);
+
+//! cart-container end
