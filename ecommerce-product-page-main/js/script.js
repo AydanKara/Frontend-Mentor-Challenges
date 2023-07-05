ce5ff26f60792f530922e1c9ba736c79e14bd340
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-07-05 22:31:01 +0200

    add product, delete product from cart update

--- ecommerce-product-page-main/js/script.js
+++ ecommerce-product-page-main/js/script.js
@@ -20,22 +20,6 @@ sidebarFunc();
 
 //! sidebar end
 
-//! cart-container start
-
-const btnCartContainer = document.querySelector(".profile-cart");
-const cartCount = document.querySelector(".cart-count");
-const cartContainer = document.querySelector("#cart-container");
-
-cartCount.style.display = "none";
-
-function cartToggleFunc() {
-  cartContainer.classList.toggle("hidden");
-}
-
-btnCartContainer.addEventListener("click", cartToggleFunc);
-
-//! cart-container end
-
 function thumbsActiveFunc() {
   const thumbs = document.querySelectorAll(".gallery-thumbs .img-fluid");
   const singleImage = document.querySelector("#single-image");
@@ -100,10 +84,7 @@ prevBtn.addEventListener("click", previousImage);
 
 function lightboxOpen() {
   const lightbox = document.querySelector(".lightbox");
-  const lightboxProductImage = document.querySelector(".lightbox-product-img");
   const closeLightbox = document.querySelector(".close-lightbox");
-  const nextLightboxBtn = document.querySelector("#next-lightbox");
-  const prevLightboxBtn = document.querySelector("#prev-lightbox");
 
   singleImage.addEventListener("click", function () {
     lightbox.style.display = "flex";
@@ -111,63 +92,9 @@ function lightboxOpen() {
   closeLightbox.addEventListener("click", function () {
     lightbox.style.display = "none";
   });
-
-  /*   function nextImageLightbox() {
-    if (currentImage === 4) {
-      currentImage = 1;
-    } else {
-      currentImage++;
-    }
-    lightboxProductImage.src = `./images/image-product-${currentImage}.jpg`;
-  }
-
-  function previousImageLightbox() {
-    if (currentImage === 1) {
-      currentImage = 4;
-    } else {
-      currentImage--;
-    }
-    lightboxProductImage.src = `./images/image-product-${currentImage}.jpg`;
-  }
-
-  nextLightboxBtn.addEventListener("click", nextImageLightbox);
-  prevLightboxBtn.addEventListener("click", previousImageLightbox);
-} */
 }
 
 lightboxOpen();
-
-/* function thumbsActiveFuncLightbox() {
-  const thumbs = document.querySelectorAll("#img-fluid-lightbox");
-  const lightboxProductImage = document.querySelector(".lightbox-product-img");
-
-  thumbs.forEach((item) => {
-    item.addEventListener("click", function () {
-      thumbs.forEach((image) => {
-        image.classList.remove("active-opacity");
-      });
-      switch (item.getAttribute("src")) {
-        case "./images/image-product-1-thumbnail.jpg":
-          lightboxProductImage.src = "./images/image-product-1.jpg";
-          break;
-        case "./images/image-product-2-thumbnail.jpg":
-          lightboxProductImage.src = "./images/image-product-2.jpg";
-          break;
-        case "./images/image-product-3-thumbnail.jpg":
-          lightboxProductImage.src = "./images/image-product-3.jpg";
-          break;
-        case "./images/image-product-4-thumbnail.jpg":
-          lightboxProductImage.src = "./images/image-product-4.jpg";
-          break;
-      }
-      item.classList.add("active-opacity");
-    });
-  });
-}
-
-thumbsActiveFuncLightbox(); */
-
-/* ------------------------- */
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -207,16 +134,75 @@ const plusBtn = document.querySelector(".btn-plus");
 const minusBtn = document.querySelector(".btn-minus");
 const quantity = document.querySelector("#quantity");
 
-let num = 0;
+let quantityValue = 0;
 
 plusBtn.addEventListener("click", () => {
-  num++;
-  quantity.innerText = num;
+  quantityValue++;
+  quantity.innerText = quantityValue;
 });
 
 minusBtn.addEventListener("click", () => {
-  if (num > 0) {
-    num--;
-    quantity.innerText = num;
+  if (quantityValue > 0) {
+    quantityValue--;
+    quantity.innerText = quantityValue;
   }
 });
+
+//! cart-container start
+
+const btnCartContainer = document.querySelector(".profile-cart");
+const cartCount = document.querySelector(".cart-count");
+const cartContainer = document.querySelector("#cart-container");
+
+cartCount.style.display = "none";
+
+function cartToggleFunc() {
+  cartContainer.classList.toggle("hidden");
+}
+
+btnCartContainer.addEventListener("click", cartToggleFunc);
+
+//! cart-container end
+
+//! add to cart start
+
+const addToCartBtn = document.querySelector(".btn-add");
+const emptyCartContent = document.querySelector(".cart-content");
+const addedProduct = document.querySelector(".added-product");
+
+addToCartBtn.addEventListener("click", () => {
+  if (quantityValue > 0) {
+    const totalPrice = 125 * quantityValue;
+    emptyCartContent.classList.add("hidden");
+    addedProduct.classList.remove("hidden");
+    addedProduct.innerHTML = `
+        <div class="product-content">
+        <img src="./images/image-product-1-thumbnail.jpg" alt="product image">
+        <div class="product-desc">
+          <p class="product-name">Fall Limited Edition Sneakers</p>
+          <p class="price">$125.00 x <span class="quantity">$${quantityValue}</span> <strong class="total">$${totalPrice.toFixed(
+      2
+    )}</strong>
+          </p>
+        </div>
+        <button class="delete-btn" onclick="deleteProduct()"><img class="delete-img" src="./images/delete-product.svg"
+            alt="delete product"></button>
+      </div>
+      <button class="checkout-btn">Checkout</button>
+        `;
+    cartCount.style.display = "flex";
+    cartCount.innerText = quantityValue;
+  }
+});
+
+//! add to cart end
+
+//! delete item from cart
+
+function deleteProduct() {
+  addedProduct.classList.add("hidden");
+  emptyCartContent.classList.remove("hidden");
+  cartCount.style.display = "none";
+}
+
+//! delete item from end
