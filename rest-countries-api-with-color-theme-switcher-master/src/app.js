d4c0041ec72b6209336f363a07a701442b8c4004
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-12-12 00:12:36 +0100

    Update app.js

--- rest-countries-api-with-color-theme-switcher-master/src/app.js
+++ rest-countries-api-with-color-theme-switcher-master/src/app.js
@@ -1,5 +1,5 @@
-import page from "../../node_modules/page/page.mjs";
-import { render } from "../../node_modules/lit-html/lit-html.js";
+import page from "../node_modules/page/page.mjs";
+import { render } from "../node_modules/lit-html/lit-html.js";
 import { layoutTemplate } from "./layout.js";
 import { homePage } from "./home.js";
 
