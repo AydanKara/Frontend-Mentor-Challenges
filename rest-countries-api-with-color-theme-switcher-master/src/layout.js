dafdf0af0c24206973e033d11df418bfc0a38fad
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-12-14 12:48:15 +0100

    color switcher update

--- rest-countries-api-with-color-theme-switcher-master/src/layout.js
+++ rest-countries-api-with-color-theme-switcher-master/src/layout.js
@@ -5,10 +5,25 @@ export const layoutTemplate = (content) => html`
     <div id="header-wrapper">
       <h1 id="header-title">Where in the world?</h1>
       <div id="color-switcher">
-        <i class="bx bx-moon"></i>
-        <span id="mode">Dark Mode</span>
+        <i class="bx bx-moon" id="icon"></i>
+        <span id="mode" @click=${onModeChange}>Dark Mode</span>
       </div>
     </div>
   </header>
   <main>${content}</main>
 `;
+
+function onModeChange() {
+  const darkMoon = document.querySelector("#icon");
+  darkMoon.classList.toggle("bxs-moon");
+  if (darkMoon.classList.contains("bxs-moon")) {
+    document.querySelector("body").style.backgroundColor =
+      "var(--Dark-Mode-Background)";
+    document.querySelector("header").style.backgroundColor = "var(--Dark-Blue)";
+    document.querySelector("#header-wrapper").style.backgroundColor =
+      "var(--Dark-Blue)";
+  } else {
+    document.querySelector("body").style.backgroundColor =
+      "var(--Light-Mode-Background)";
+  }
+}
