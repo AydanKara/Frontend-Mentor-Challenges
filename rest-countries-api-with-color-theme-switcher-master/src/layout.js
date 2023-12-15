89c969f98455b6151c82f2cc9fd7218d1aad2ae6
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-12-15 10:32:06 +0100

    dark mode configurations

--- rest-countries-api-with-color-theme-switcher-master/src/layout.js
+++ rest-countries-api-with-color-theme-switcher-master/src/layout.js
@@ -19,9 +19,38 @@ function onModeChange() {
   if (darkMoon.classList.contains("bxs-moon")) {
     document.querySelector("body").style.backgroundColor =
       "var(--Dark-Mode-Background)";
+    document.querySelector("#header-title").style.color = "var(--White)";
+    document.querySelector("#color-switcher i").style.color = "var(--White)";
+    document.querySelector("#color-switcher #mode").style.color =
+      "var(--White)";
+
+    document.querySelector("#search-control i").style.color = "var(--White)";
+    document.querySelector("#filter-control p").style.color = "var(--White)";
+    document.querySelectorAll("li.regions").forEach((region) => {
+      region.style.color = "var(--White)";
+    });
+    document.querySelector("#dropdown i").style.color = "var(--White)";
+    document.querySelector("#search-bar").style.backgroundColor =
+      "var(--Dark-Blue)";
+    document.querySelector("#filter-control #dropdown").style.backgroundColor =
+      "var(--Dark-Blue)";
+    document.querySelector("#filter-control .options").style.backgroundColor =
+      "var(--Dark-Blue)";
     document.querySelector("header").style.backgroundColor = "var(--Dark-Blue)";
     document.querySelector("#header-wrapper").style.backgroundColor =
       "var(--Dark-Blue)";
+    document.querySelectorAll(".country-info").forEach((country) => {
+      country.style.backgroundColor = "var(--Dark-Blue)";
+    });
+    document.querySelectorAll(".country-info h2").forEach((country) => {
+      country.style.color = "var(--White)";
+    });
+    document.querySelectorAll(".country-info p").forEach((country) => {
+      country.style.color = "var(--White)";
+    });
+    document.querySelectorAll(".country-info p strong").forEach((country) => {
+      country.style.color = "var(--White)";
+    });
   } else {
     document.querySelector("body").style.backgroundColor =
       "var(--Light-Mode-Background)";
