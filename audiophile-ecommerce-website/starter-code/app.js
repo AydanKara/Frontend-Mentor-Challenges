793025286b4b8323e2c274294bb5c08f66c39682
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-12-23 22:46:25 +0100

    settings

--- audiophile-ecommerce-website/starter-code/app.js
+++ audiophile-ecommerce-website/starter-code/app.js
@@ -1,6 +1,9 @@
+const serverless =  require("serverless-http");
+
 const path = require("path");
 const express = require("express");
 const authRoutes = require("./routes/auth.routes");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -8,3 +11,5 @@ app.set("views", path.join(__dirname, "views"));
 app.use(authRoutes);
 
 app.listen(3000);
+
+const handler = serverless(app);
