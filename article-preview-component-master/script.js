3e584ea30be9a157a0cb23a8aa1004c6e5c62f20
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-04-13 18:36:12 +0200

    Finishing the project
    Adding Responsive Design

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -2,5 +2,5 @@ const shareButton = document.getElementById("share-btn");
 const shareTooltip = document.getElementById("tooltip");
 
 shareButton.addEventListener("click", () => {
-    shareTooltip.classList.toggle("active");
-})
+  shareTooltip.classList.toggle("active");
+});
